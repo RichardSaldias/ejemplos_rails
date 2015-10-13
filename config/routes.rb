87fd123952a09c2ed2f3764c15f57c105c8d01af
Petirrojo <resus.be@gmail.com>
2015-10-12 21:21:07 -0400

    probado scaffold 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+  
+  resources :faculties
   get 'welcome/index'
+  root 'welcome#index'
+  get "inicio", to: "welcome#index"
+
+
+
+
+
+  #get "nuevaruta", to: "controlador#accion"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
